19ed87abe229b6335ee48f7a0429a2ed4f6d12ab
Michael Desa <mjdesa@gmail.com>
2019-11-19 11:32:36 -0500

    feat: first pass at migrating dashboards

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/desa/chronograf-migrator
+
+go 1.12
+
+require github.com/influxdata/chronograf v0.0.0-20191108211352-0b0e4b8bf091
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/influxdata/chronograf"
+	"github.com/influxdata/chronograf/bolt"
+)
+
+func main() {
+	c := bolt.NewClient()
+	c.Path = "/Users/michaeldesa/go/src/github.com/desa/chronograf-migrator/chronograf-v1.db"
+
+	ctx := context.Background()
+
+	if err := c.Open(ctx, nil, chronograf.BuildInfo{}); err != nil {
+		panic(err)
+	}
+
+	dashboardStore := c.DashboardsStore
+
+	ds, err := dashboardStore.All(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, d := range ds {
+		fmt.Println(d.Name)
+	}
+}
